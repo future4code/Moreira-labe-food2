4f73d121dc356bd020fe66e1ceef8593279e838f
Atauan Lima <atauan198@gmail.com>
2022-03-14 14:29:31 -0300

    Adcionando global Styled

---
+++ src/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const token = localStorage.getItem('token');
+
+export const api = axios.create({
+  baseURL: 'https://us-central1-missao-newton.cloudfunctions.net/',
+  headers: {
+    auth: token,
+  }
+});
