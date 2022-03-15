2861359a17e5c184ec7ce1ed1611c42a9c715474
ninazorzanello <carina.zorzanello@gmail.com>
2022-03-15 10:52:11 -0300

    finalizando css paginas

--- src/GlobalStyled.js
+++ src/GlobalStyled.js
@@ -2,22 +2,10 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyled = createGlobalStyle `
 body {      
-  font-family: Roboto, sans-serif;   
-  display: flex;   
-  flex-direction: column;   
-  margin: 50px auto;   
-  width: 100%;   
-  height: 50vh;   
-  box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px; 
+  font-family: Roboto, sans-serif;  
 }  
-   @media screen and (max-width: 960px)
-   {     
-    margin: 0;    
-    width: 100%;    
-    height: 100vh;        
-  } 
-  * 
-  {   
+
+ * {   
     margin: 0;   
     box-sizing: border-box; 
   }
