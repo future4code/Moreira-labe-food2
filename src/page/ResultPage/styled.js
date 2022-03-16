82338dd706126eae290f402980f2136724322849
jaime-github <jaime-epifanio@hotmail.com>
2022-03-15 21:59:10 -0300

    Tela de Detalhes do Restaurante-1

---
+++ src/page/ResultPage/styled.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding-top: 50px;
+`
+export const Img = styled.img`
+    width: 90%;
+`
+export const Card = styled.div`
+    margin: 0 auto;
+    background: #FFFFFF;
+    width: 80%;
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+    border-radius: 10px;
+`
+export const Input = styled.input`
+    padding: 20px 20px 20px 40px;
+    width: 100%;
+    border: 1px solid #b6b6b6;
+    border-radius: 4px;
+`
+export const Ul = styled.ul`
+    list-style: none;
+    margin: 30px 0 100px 0 ;
+`
+export const Li = styled.li`
+    margin: 10px;
+    
+`
+export const ContainerList = styled.div`
+    
+    margin: 10px;
+    
+`
+export const IconSearch = styled.p`
+    position: absolute;
+    top: 22px; 
+    left: 10px;
+    color: #000000;
+    opacity: 25%;
+`
+export const Title = styled.div`
+    font-size: 17px;
+    letter-spacing: -0.39px;
+    border: 0;
+`
