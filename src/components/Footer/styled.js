f7d5c4d96e4f957492711dfb0ced51d7a9e52d18
HEINRICK7 <heinrickcostta@gmail.com>
2022-03-15 21:36:59 -0300

    upddate pages

---
+++ src/components/Footer/styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    position:fixed;
+    bottom: 0;
+    height: 50px;
+    border: 0;
+    border-top: 1px solid #b6b6b6;
+    background-color: #FFFFFF;
+
+ul {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    height: 50px;
+}
+li {
+    font-size: 27px;
+    color: #000000;
+    opacity: 25%;
+}
+
+`
