2c321228a3717461038d4cb655575c8ba3d0e8fe
HEINRICK7 <heinrickcostta@gmail.com>
2022-03-16 17:33:30 -0300

    feddRestaurants

--- src/page/FeedPage/styled.js
+++ src/page/FeedPage/styled.js
@@ -8,10 +8,14 @@ export const Container = styled.div`
     padding-top: 50px;
 `
 export const Img = styled.img`
-    width: 90%;
+    width: 100%;
+    max-height: 40vh;
+    min-height: 30vh;
+    object-fit: center;
+    border-radius: 10px 10px 0 0;
 `
 export const Card = styled.div`
-    margin: 0 auto;
+    margin: 20px auto;
     background: #FFFFFF;
     width: 80%;
     box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
@@ -28,7 +32,7 @@ export const Ul = styled.ul`
     margin: 30px 0 100px 0 ;
 `
 export const Li = styled.li`
-    margin: 10px;
+    
     
 `
 export const ContainerList = styled.div`
