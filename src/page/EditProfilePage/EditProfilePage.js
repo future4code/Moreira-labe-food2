0be5e0f98494b603aafec256b8e5135d141b59c1
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2022-03-18 12:21:36 -0300

    paginas do perfil e editar

--- src/page/EditProfilePage/EditProfilePage.js
+++ src/page/EditProfilePage/EditProfilePage.js
@@ -5,10 +5,11 @@ import GlobalStateContext from "../../global/GlobalStateContext"
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import {BASE_URL} from "../../constants/Urls";
-import {Input, Form, DivForm} from './styled';
+import {Input, Form, DivForm, Header, Buttons, Container} from './styled';
 import useForm from '../../hooks/useForm';
 import Button from "@mui/material/Button";
-
+import {IoIosArrowBack} from 'react-icons/io';
+import { goToProfile } from "../../routes/coordinator";
 
 export default function EditProfilePage() {
   useProtectedPage()
@@ -20,7 +21,7 @@ export default function EditProfilePage() {
   useEffect(() => {  
     axios.get(`${BASE_URL}/profile`, token)
         .then(response => {
-          setters.setPerfil(response.data.user)
+          setters.setProfile(response.data.user)
         })
         .catch((error) => {
             console.log(error.response.data)
@@ -37,6 +38,7 @@ export default function EditProfilePage() {
   const onSubmitForm = (event) => {
     event.preventDefault()
     putUpdateProfile() //alteração
+    goToProfile(navigate)
   }
 
 
@@ -46,7 +48,7 @@ export default function EditProfilePage() {
 
     axios.put(`${BASE_URL}/profile`, body,  token)
         .then(response => {
-          setters.setPerfil(response.data.user)
+          setters.setProfile(response.data.user)
           console.log("Dados alterados com sucesso!")
           console.log(response.data.user)
         })
@@ -59,20 +61,20 @@ export default function EditProfilePage() {
 
   return (
 
-    <div>
-      <div> 
-        <p>{states.perfil.name}</p>
-        <p>{states.perfil.email}</p>
-        <p>{states.perfil.cpf}</p>
+    <Container>
 
-      </div>
+      <Header>
+      <Buttons onClick={()=>{navigate(-1)}}><IoIosArrowBack color="black" fontSize="20"/></Buttons>
+        <p className="title">Editar</p>
+      </Header>
+      
       <DivForm>
       <Form onSubmit={onSubmitForm}>
 
                         <Input 
                             name={"name"}
                             label={"Nome"}
-                            placeholder={states.perfil.name} /* alteração */
+                            placeholder={states.profile.name} /* alteração */
                             value={form.name} /* alteração */
                             type="text"
                             required
@@ -91,7 +93,7 @@ export default function EditProfilePage() {
                             name={"email"}
                             label={"E-mail"}
                             value={form.email}
-                            placeholder={states.perfil.email}
+                            placeholder={states.profile.email}
                             onChange={onChange}
                             required
                             type="email"
@@ -112,7 +114,7 @@ export default function EditProfilePage() {
                             onChange={onChange}
                             required
                             pattern="^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}"
-                            placeholder={states.perfil.cpf}
+                            placeholder={states.profile.cpf}
                             variant={"outlined"}
                             id="outlined-required"
                             defaultValue="CPF"
@@ -128,15 +130,11 @@ export default function EditProfilePage() {
                             variant="contained"
                             color="primary"
                             >
-                                Criar
+                                Salvar
                         </Button>
                     </Form>
             </DivForm>
- 
-{/* 
-    <Link to='/login'>Login</Link> */}
-
-    </div>
+    </Container>
   )
 }
 
